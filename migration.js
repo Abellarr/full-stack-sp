6f905c8c4e8c38e18fb59ad09d99e4df631a75c8
Abellarr <da.ortiz83@gmail.com>
2023-03-03 14:51:10 -0800

    Updated migration and seed files to properly deploy into remote database

--- migration.js
+++ migration.js
@@ -2,31 +2,42 @@ const { Pool } = require('pg');
 const DATABASE_URL = process.env.DATABASE_URL;
 const pool = require('./connectDB.js');
 
-pool.query(`CREATE TABLE IF NOT EXISTS npc_type (
-    id SERIAL PRIMARY KEY,
-    npc_type TEXT);`, 
-    (err, data)=>{
-        if (err){
-            console.log('NPC Type Table failed')
-        } else {
-            console.log(data);
-            pool.query(`CREATE TABLE IF NOT EXISTS npc_char (
-                id SERIAL PRIMARY KEY,
-                char_name TEXT,
-                race VARCHAR(30),
-                class VARCHAR(50),
-                hit_points INTEGER,
-                background TEXT,
-                type_id INTEGER,
-                CONSTRAINT FK_npc_char FOREIGN KEY (type_id) REFERENCES npc_type(id) ON UPDATE CASCADE);`,
-                (err, data)=>{
-                    if (err){
-                        console.log('NPC Character Table failed');
-                    } else {
-                        console.log(data);
-                    }
-                })
-        }
-    })
+pool.query(`DROP TABLE IF EXISTS npc_type;`, (err, data)=>{
+    if (err){
+        console.log('Drop NPC Type Table failed')
+    } else {
+        pool.query('CREATE TABLE npc_type (id SERIAL PRIMARY KEY, npc_type TEXT);', (err, data)=>{
+            if (err){
+                console.log('Create NPC Type Table failed');
+            } else {
+                console.log('Create NPC Type Table successful')
+            }
+         });
+     }
+})
+
+pool.query(`DROP TABLE IF EXISTS npc_char;`, (err, data)=>{
+    if (err){
+        console.log('Drop NPC Char Table failed');
+    } else {
+        pool.query(`CREATE TABLE npc_char (
+            id SERIAL PRIMARY KEY,
+            char_name TEXT,
+            race VARCHAR(30),
+            class VARCHAR(50),
+            hit_points INTEGER,
+            background TEXT,
+            type_id INTEGER,
+            CONSTRAINT FK_npc_char FOREIGN KEY (type_id) REFERENCES npc_type(id) ON UPDATE CASCADE);`,
+            (err, data)=>{
+                if (err){
+                    console.log('Create NPC Char Table failed');
+                } else {
+                    console.log('Create NPC Char Table successful');
+                }
+        })
+    }
+})
+
 
 pool.end();
--- seed.js
+++ seed.js
@@ -1,49 +1,43 @@
 const { Pool } = require('pg');
+const DATABASE_URL = process.env.DATABASE_URL;
 const pool = require('./connectDB.js');
 
-pool.query('SELECT COUNT(*) FROM npc_type;', (err, data)=>{
+
+pool.query(`INSERT INTO npc_type (npc_type) VALUES ('Combat'), ('Commoner'), ('Nobility'), ('Big Bad Boss'), ('Quest Contact'), ('Patron');`, (err, data)=>{
     if (err){
-        console.log('Seed failed start');
-    }
-    if (data.rows[0].count == 0){
-        pool.query(`INSERT INTO npc_type (npc_type) VALUES ('Combat'), ('Commoner'), ('Nobility'), ('Big Bad Boss'), ('Quest Contact'), ('Patron');`, (err, data)=>{
-            if (err){
-                console.log('NPC Type Table failed')
-            } else {
-                console.log(data);
-                pool.query('SELECT COUNT(*) FROM npc_char;', (err, data)=>{
-                    if (err){
-                        console.log('Seed failed second');
-                    }
-                    if (data.rows[0].count == 0){
-                        pool.query(`INSERT INTO npc_char (char_name, race, class, hit_points, background, type_id) VALUES 
-                            ('Beldrath', 'Half-Elf', 'Paladin', 125, 'Urban Bounty Hunter', 1),
-                            ('Minthrad', 'Human', 'Arch-Wizard', 200, 'Sage', 6),
-                            ('Minerva', 'Halfling', 'Tavern Hostess', 10, 'Outlander', 2),
-                            ('Terath', 'Half-Orc', 'Innkeeper', 10, 'Urchin', 2),
-                            ('Chard', 'Human', 'Shopkeeper', 10, 'Soldier', 2),
-                            ('Byorg', 'Human', 'Barbarian', 175, 'Outlander', 1),
-                            ('Valerea', 'Elf', 'Sorceror', 50, 'Charlatan', 1),
-                            ('Mordrand', 'Dwarf', 'Cleric', 125, 'Acolyte', 1),
-                            ('Chauncey', 'Human', 'Mayor', 10, 'Noble', 3),
-                            ('Lord Farci', 'Human', 'Duke of Wellingshire', 10, 'Noble', 3),
-                            ('Vendril', 'Lich', 'Arch-Lich Wizard', 500, 'Demigod', 4),
-                            ('Gnarthon', 'Ogre', 'Fighter', 160, 'Soldier', 4),
-                            ('Reginald', 'Halfling', 'Rogue', 60, 'Harper Agent', 5),
-                            ('Calenor', 'Half-Elf', 'Cleric', 75, 'Acolyte', 5),
-                            ('Balgethron', 'Drow Elf', 'Wizard', 175, 'Red Wizard', 6);`, (err, data)=>{
-                            if (err){
-                                console.log('NPC Character Table failed');
-                            } else {
-                                console.log(data);
-                            }
-                        })
-                    }
-                })
-            }
-        })
+        console.log('NPC Type Seed failed')
+    } else {
+        console.log('NPC Type Seed Successful');
+        console.log(data);
     }
 })
+
+pool.query(`INSERT INTO npc_char (char_name, race, class, hit_points, background, type_id) VALUES 
+    ('Beldrath', 'Half-Elf', 'Paladin', 125, 'Urban Bounty Hunter', 1),
+    ('Minthrad', 'Human', 'Arch-Wizard', 200, 'Sage', 6),
+    ('Minerva', 'Halfling', 'Tavern Hostess', 10, 'Outlander', 2),
+    ('Terath', 'Half-Orc', 'Innkeeper', 10, 'Urchin', 2),
+    ('Chard', 'Human', 'Shopkeeper', 10, 'Soldier', 2),
+    ('Byorg', 'Human', 'Barbarian', 175, 'Outlander', 1),
+    ('Valerea', 'Elf', 'Sorceror', 50, 'Charlatan', 1),
+    ('Mordrand', 'Dwarf', 'Cleric', 125, 'Acolyte', 1),
+    ('Chauncey', 'Human', 'Mayor', 10, 'Noble', 3),
+    ('Lord Farci', 'Human', 'Duke of Wellingshire', 10, 'Noble', 3),
+    ('Vendril', 'Lich', 'Arch-Lich Wizard', 500, 'Demigod', 4),
+    ('Gnarthon', 'Ogre', 'Fighter', 160, 'Soldier', 4),
+    ('Reginald', 'Halfling', 'Rogue', 60, 'Harper Agent', 5),
+    ('Calenor', 'Half-Elf', 'Cleric', 75, 'Acolyte', 5),
+    ('Balgethron', 'Drow Elf', 'Wizard', 175, 'Red Wizard', 6);`, (err, data)=>{
+    if (err){
+        console.log('NPC Char Seed failed');
+    } else {
+        console.log('NPC Char Seed successful')
+        console.log(data);
+    }
+})
+ 
+
+
 
 
 pool.end();
