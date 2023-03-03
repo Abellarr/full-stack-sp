134447ac16b86a0c70f5922387fd8afb0acafc82
Abellarr <da.ortiz83@gmail.com>
2023-03-03 15:45:59 -0800

    Fix 4

--- migration.js
+++ migration.js
@@ -12,40 +12,36 @@ pool.query(`DROP TABLE IF EXISTS npc_type`, (err, data)=>{
     if (err){
         console.log('Drop NPC Type Table failed')
     }
+    pool.query(`CREATE TABLE npc_type (
+        id serial PRIMARY KEY,
+        npc_type text)`, (err, data)=>{
+        if (err){
+            console.log('Create NPC Type Table failed');
+        } else {
+            console.log('Create NPC Type Table successful');
+        }
+    })
 })
 
 pool.query(`DROP TABLE IF EXISTS npc_char`, (err, data)=>{
     if (err){
         console.log('Drop NPC Char Table failed');
     }
-})
-
-
-pool.query(`CREATE TABLE npc_type (
-    id serial PRIMARY KEY,
-    npc_type text)`, (err, data)=>{
-    if (err){
-        console.log('Create NPC Type Table failed');
-    } else {
-        console.log('Create NPC Type Table successful');
-    }
-});
-
-
-pool.query(`CREATE TABLE npc_char (
-    id serial PRIMARY KEY,
-    char_name text,
-    race varchar(30),
-    class varchar(50),
-    hit_points integer,
-    background text,
-    type_id integer,
-    FOREIGN KEY (type_id) REFERENCES npc_type(id) ON UPDATE CASCADE)`, (err, data)=>{
-        if (err){
-            console.log('Create NPC Char Table failed');
-        } else {
-            console.log('Create NPC Char Table successful');
-        }
+    pool.query(`CREATE TABLE npc_char (
+        id serial PRIMARY KEY,
+        char_name text,
+        race varchar(30),
+        class varchar(50),
+        hit_points integer,
+        background text,
+        type_id integer,
+        FOREIGN KEY (type_id) REFERENCES npc_type(id) ON UPDATE CASCADE)`, (err, data)=>{
+            if (err){
+                console.log('Create NPC Char Table failed');
+            } else {
+                console.log('Create NPC Char Table successful');
+            }
+    })
 })
 
 
