ae04d8db18a5de2bb5db9f9f002c70d74115899b
Abellarr <da.ortiz83@gmail.com>
2023-03-03 14:59:33 -0800

    Altered migration.js for deployment

--- migration.js
+++ migration.js
@@ -6,7 +6,10 @@ pool.query(`DROP TABLE IF EXISTS npc_type;`, (err, data)=>{
     if (err){
         console.log('Drop NPC Type Table failed')
     } else {
-        pool.query('CREATE TABLE npc_type (id SERIAL PRIMARY KEY, npc_type TEXT);', (err, data)=>{
+        pool.query(`CREATE TABLE npc_type (
+            id serial PRIMARY KEY,
+            npc_type text
+            );`, (err, data)=>{
             if (err){
                 console.log('Create NPC Type Table failed');
             } else {
@@ -21,15 +24,15 @@ pool.query(`DROP TABLE IF EXISTS npc_char;`, (err, data)=>{
         console.log('Drop NPC Char Table failed');
     } else {
         pool.query(`CREATE TABLE npc_char (
-            id SERIAL PRIMARY KEY,
-            char_name TEXT,
-            race VARCHAR(30),
-            class VARCHAR(50),
-            hit_points INTEGER,
-            background TEXT,
-            type_id INTEGER,
-            CONSTRAINT FK_npc_char FOREIGN KEY (type_id) REFERENCES npc_type(id) ON UPDATE CASCADE);`,
-            (err, data)=>{
+            id serial PRIMARY KEY,
+            char_name text,
+            race varchar(30),
+            class varchar(50),
+            hit_points integer,
+            background text,
+            type_id integer,
+            CONSTRAINT FK_npc_char FOREIGN KEY (type_id) REFERENCES npc_type(id) ON UPDATE CASCADE
+            );`, (err, data)=>{
                 if (err){
                     console.log('Create NPC Char Table failed');
                 } else {
